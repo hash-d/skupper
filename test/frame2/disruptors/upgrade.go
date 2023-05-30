d820e52e80c0047f7289c4efb2163dff68027768
Danilo Gonzalez Hashimoto <97529980+hash-d@users.noreply.github.com>
2023-05-30 18:00:02 -0300

    Implemented ENV_UPGRADE_STRATEGY with :INVERT

--- test/frame2/disruptors/upgrade.go
+++ test/frame2/disruptors/upgrade.go
@@ -1,7 +1,9 @@
 package disruptors
 
 import (
+	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/skupperproject/skupper/test/frame2"
@@ -9,10 +11,54 @@ import (
 	"github.com/skupperproject/skupper/test/utils/base"
 )
 
+// Sort the targets according to some strategy, configured on
+// SKUPPER_TEST_UPGRADE_STRATEGY.  If none set, return the
+// target list unchanged
+func sortTargets(targets []*base.ClusterContext) []*base.ClusterContext {
+
+	var ret []*base.ClusterContext
+	var invert bool
+	var strategy frame2.TestUpgradeStrategy
+
+	envValue := os.Getenv(frame2.ENV_UPGRADE_STRATEGY)
+
+	s := strings.SplitN(envValue, ":", 2)
+	strategy = frame2.TestUpgradeStrategy(s[0])
+	if strategy == "" {
+		strategy = frame2.UPGRADE_STRATEGY_CREATION
+	}
+	if len(s) > 1 {
+		if s[1] == string(frame2.UPGRADE_STRATEGY_INVERSE) {
+			invert = true
+		} else {
+			panic(fmt.Sprintf("invalid option to SKUPPER_TEST_UPGRADE_STRATEGY: %v", s[1]))
+		}
+	}
+
+	switch strategy {
+	case frame2.UPGRADE_STRATEGY_CREATION:
+		ret = targets[:]
+	default:
+		panic(fmt.Sprintf("invalid upgrade strategy: %v", strategy))
+	}
+
+	if invert {
+		lenRet := len(ret)
+		for i := 0; i < lenRet/2; i++ {
+			ret[i], ret[lenRet-i-1] = ret[lenRet-i-1], ret[i]
+		}
+	}
+
+	return ret
+}
+
 // At the end of the test, before the tear down, upgrade all
 // sites and then re-run all tests marked as final
 //
 // This is a very basic upgrade test; it's cheap and simple
+//
+// The upgrade strategy can be defined on the environment
+// variable SKUPPER_TEST_UPGRADE_STRATEGY.
 type UpgradeAndFinalize struct {
 	targets []*base.ClusterContext
 	useNew  bool
@@ -26,7 +72,9 @@ func (u *UpgradeAndFinalize) PreFinalizerHook(runner *frame2.Run) error {
 	var steps []frame2.Step
 	u.useNew = true
 
-	for _, t := range u.targets {
+	targets := sortTargets(u.targets)
+
+	for _, t := range targets {
 		steps = append(steps, frame2.Step{
 			Modify: execute.SkupperUpgrade{
 				Runner:    runner,
--- test/frame2/env.go
+++ test/frame2/env.go
@@ -2,10 +2,49 @@ package frame2
 
 // Frame2-specific environment variables
 
+type TestUpgradeStrategy string
+
 const (
 	// This sets the 'Allow' parameter of the retry block for the final
 	// validations, and needs to be an integer value.  Any validations
 	// marked as final will be retried this many times at the end of the
 	// test.
 	ENV_FINAL_RETRY = "SKUPPER_TEST_FINAL_RETRY"
+
+	// Define the upgrade strategy used by the Upgrade disruptor (possibly
+	// other points as well?)
+	//
+	// Values:
+	//
+	// CREATION (default): order of skupper init
+	// PUB_FIRST
+	// PRV_FIRST
+	// PUB_ONLY
+	// PRV_ONLY
+	// LEAVES_FIRST
+	// LEAVES_ONLY
+	// CORE_FIRST
+	// CORE_ONLY
+	// EDGES_FIRST
+	// EDGES_ONLY
+	// INTERIOR_FIRST
+	// INTERIOR_ONLY
+	//
+	// Currently, only CREATION and CREATION:INVERSE are implemented
+	//
+	// For any of the options, if the value ends with :INVERSE, the order
+	// is inverted.  For example, ":INVERSE" or "CREATION:INVERSE" will
+	// upgrade the lastly installed skupper site first; the first last.
+	//
+	// Valid values are of the string type TestUpgradeStrategy
+	ENV_UPGRADE_STRATEGY = "SKUPPER_TEST_UPGRADE_STRATEGY"
+)
+
+// Upgrade strategies accepted by ENV_UPGRADE_STRATEGY
+const (
+	UPGRADE_STRATEGY_CREATION TestUpgradeStrategy = "CREATION"
+
+	// This one is special; it is set after a colon and inverts the
+	// result. For example: ":INVERSE" or "CREATION:INVERSE"
+	UPGRADE_STRATEGY_INVERSE TestUpgradeStrategy = "INVERSE"
 )
